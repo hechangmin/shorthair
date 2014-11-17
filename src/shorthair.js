7a3690476b793207e2a5070e7a2bb724d8c93654
takumi4ichi <takumi4ichi@gmail.com>
2014-11-17 21:35:40 +0800

    修改lex正则定义顺序

--- src/shorthair.js
+++ src/shorthair.js
@@ -12,20 +12,21 @@ var shorthair = (function(){
 
 
     //css3-selector基本正则定义
-    var ident = jcon.regex(/[-]?/).seq(nmstart,nmchar.many());
-    var name = nmchar.least(1);
-    var nmstart = jcon.regex(/[_a-z]/).or(nonascii, escape);
     var nonascii = jcon.regex(/[^\0-\177]/);
     var unicode = jcon.regex(/\\[0-9a-f]{1,6}(\r\n|[ \n\r\t\f])?/);
-    var escape = unicode.or(jcon.regex(/\\[^\n\r\f0-9a-f]/));
     var nmchar = jcon.regex(/[_a-z0-9-]/).or(nonascii, escape);
+    var nmstart = jcon.regex(/[_a-z]/).or(nonascii, escape);
+    var ident = jcon.regex(/[-]?/).seq(nmstart,nmchar.many());
+    var name = nmchar.least(1);
+    var escape = unicode.or(jcon.regex(/\\[^\n\r\f0-9a-f]/));
     var num = jcon.regex(/[0-9]+|[0-9]*\.[0-9]+/);
-    var string = string1.or(string2);
     var nl = jcon.regex(/\n|\r\n|\r|\f/);
     var string1 = jcon.string('"').seq(jcon.regex(/[^\n\r\f\\"]/).or(nl, nonascii, escape).many(), jcon.string('"'));
     var string2 = jcon.string("'").seq(jcon.regex(/[^\n\r\f\\']/).or(nl, nonascii, escape).many(), jcon.string('"'));
+    var string = string1.or(string2);
     var invalid1 = jcon.string('"').seq(jcon.regex(/[^\n\r\f\\"]/).or(nl, nonascii, escape).many());
     var invalid2 = jcon.string("'").seq(jcon.regex(/[^\n\r\f\\']/).or(nl, nonascii, escape).many());
+    var invalid = invalid1.or(invalid2);
     var w = jcon.regex(/[ \t\r\n\f]*/);
 
     //var D = d.or(jcon.regex(/\\0{0,4}(44|64)(\r\n|[ \t\r\n\f])?/));
@@ -48,7 +49,7 @@ var shorthair = (function(){
     var GREATER = w.seq(jcon.string('>'));
     var COMMA = w.seq(jcon.string(','));
     var TILDE = w.seq(jcon.string('~'));
-    var NOT = jcon.string(':').seq(n, o, t, jcon.string('('));
+    var NOT = jcon.string(':not(');
     var ATKEYWORD = jcon.string('@').seq(ident);
     var INVALID = invalid;
     var PERCENTAGE = num.seq(jcon.string('%'));
